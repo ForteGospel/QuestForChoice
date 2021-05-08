1b54c2d5ff77c0cc9d7ba13cea43d0a7c11c42d0
osher volotker <volotkero@gmail.com>
2021-05-08 21:12:49 +0300

    player creation and score

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Score : MonoBehaviour
+{
+
+    public Text MyscoreText;
+    private int ScoreNum;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ScoreNum = 0;
+        MyscoreText.text = "Score : " + ScoreNum;
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D coin)
+    {
+        if (coin.tag == "Mycoin")
+        {
+            ScoreNum += 1;
+            Destroy(coin.gameObject);
+            MyscoreText.text = "Score:" + ScoreNum;
+        }
+    }
+
+
+
+
+
+}
---
+++ Assets/Scripts/weaponshoot.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class weaponshoot : MonoBehaviour
+{
+    public float offset;
+    public GameObject projectile;
+    public Transform shotPoint;
+    private float timeBtwShots;
+    public float startTimeBtwShots;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+
+    private void Update()
+    {
+        Vector3 diffrence = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        float rotZ = Mathf.Atan2(diffrence.y, diffrence.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, rotZ + offset);
+
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(projectile, shotPoint.position, transform.rotation);
+        }
+
+        if (timeBtwShots <= 0)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Instantiate(projectile, shotPoint.position, transform.rotation);
+                timeBtwShots = startTimeBtwShots;
+            }
+        }
+
+        else{
+            timeBtwShots -= Time.deltaTime;
+        }
+
+
+    }
+
+
+
+
+
+
+
+}

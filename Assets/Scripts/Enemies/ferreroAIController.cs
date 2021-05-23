8aad934627ac6cc982f57d5bfe8aa36431289afb
Jonathan Quiben <54368132+ForteGospel@users.noreply.github.com>
2021-05-23 18:14:24 +0300

    bouncy platform, spikes, enemy wait for activation, flying enemy

--- Assets/Scripts/Enemies/ferreroAIController.cs
+++ Assets/Scripts/Enemies/ferreroAIController.cs
@@ -22,6 +22,6 @@ public class ferreroAIController : twixAIController
 
     private new void OnDrawGizmos()
     {
-        
+        Gizmos.DrawWireSphere(transform.position, distanceToActivate);
     }
 }
